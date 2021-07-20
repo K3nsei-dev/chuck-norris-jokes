bfa51078bbc587b1ac1aede3143424227947f78c
Jeandre Awood <jeandreross@gmail.com>
2021-07-20 11:07:00 +0200

    did styling and added picture

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 
@@ -10,7 +10,15 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
 
-    return "<strong>Random Joke From Chuck Norris:</strong>" + " " + response['value']
+    return render_template('home.html', api_url=api_url, response=response['value'])
+
+
+@app.route('/apidata')
+def api():
+    api_url = "https://api.chucknorris.io/jokes/random"
+    response = requests.get(api_url).json()
+
+    return render_template('api.html', api_url=api_url, response=response)
 
 
 if __name__ == '__main__':
