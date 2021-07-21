2af08de8ff76334c8ee863fd5a0bdae08044fecc
Jeandre Awood <jeandreross@gmail.com>
2021-07-21 14:15:53 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
-
 
 app = Flask(__name__)
 
@@ -19,6 +18,14 @@ def api():
     response = requests.get(api_url).json()
 
     return render_template('api.html', api_url=api_url, response=response)
+
+
+@app.route('/categories')
+def categories():
+    api_url = "https://api.chucknorris.io/jokes/categories"
+    response = requests.get(api_url).json()
+
+    return render_template('categories.html', response=jsonify(response))
 
 
 if __name__ == '__main__':
